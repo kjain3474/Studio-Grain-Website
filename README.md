4ff4ef6362e83d6a3de2cc8220ba8a12a90186ff
kjain3474 <kjain3474@gmail.com>
2020-07-28 20:59:13 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Studio-Grain-Website
 Bootstrap 4 Website for the Company Studio Grain 
+
+**Live Website** : www.studiograin.com
---
+++ project/js/projMap.js
@@ -0,0 +1,79 @@
+var projMap = 
+{
+
+	"Bhavya Ramesh":{
+		'title': "Bhavya Ramesh",
+		'desc': "Bhavya Ramesh is a unique silver jewelry brand, who’s line of products are strongly defined by natural elements. Bold and edgy are two words to describe the brand and their products.\
+In accordance to that, we created a distinct logo form for the brand, with bold colors of red and grey representing its form. We have incorporated an upward arrow in the logo, inspired by vintage Indian metal ornaments & an arrow is also synonymous of movement and structure, much like the jewelry Bhavya Ramesh creates.",
+		'length' : 9
+	},
+
+	"Muskan _ Chirag":{
+		'title': "Muskan & Chirag",
+		'desc': "Inspired by palatial opulence, architecture and a courtyard setting, this wedding invite was designed for a traditional affair. The wedding venue being Mysore, a lot of inspiration was drawn from tropical foliage & intricate details of a South Indian Palace, christened with the grandeur of quintessential Mysore elephants.\
+ We designed the invite card with inserts and an envelope, stationery for the couple and a few giveaways at the wedding.",
+		'length' : 8
+	},
+
+	"Priyanka _ Yash":{
+		'title': "Priyanka & Yash",
+		'desc': "A Rajasthani wedding, hence a lot of inspiration was drawn from the checkered and jaali patterns which usually ornament a traditional Rajasthani door. As the wedding took place in spring and was by the beach, the colour palette is different pastel shades, adorned with the bride and grooms favourite flowers (the mogra and the rose). The design was a mix of traditional and modern.\
+		The same uniformity was then followed in the stationery design as well.",
+		'length' : 14
+	},
+
+	"Icarus":{
+		'title': "Icarus",
+		'desc': "Branding & Identity deisgned for a Software Development firm, that primarily deals in the programming of drones for industrial use and its functions. Icarus means aritificial wings, this definition reflects in the logo form and branding as well.",
+		'length' : 5
+	},
+
+	"Komal _ Kritish":{
+		'title': "Komal & Kritish",
+		'desc': "The couple went for a simple wedding invite. They were particular about wedding elements adorning this minimal but colourful card. We have created a minimal Indian wedding invite, with Indian patterns across the invite cards.",
+		'length' : 6
+	},
+
+	"Vishal _ Nidhi":{
+		'title': "Vishal & Nidhi",
+		'desc': "A fun take on Indian wedding functions, this was a completely illustrated invite, which was digitally sent out to all the guests at the venue. The colours are bright representing the big fat Indian wedding. The illustrations are hand drawn, inspired by real wedding events. We had a total blast designing the e-invite and stationery for this wedding.",
+		'length' : 8
+	},
+
+	"Vintage Botanicals":{
+		'title': "Vintage Botanicals",
+		'desc': "Illustrated vintage botanicals for a client who had a penchant for tropical stationery. These were digitally created and designed to suit the customised stationery set.",
+		'length' : 6
+	},
+
+
+	"Stippled Stories":{
+		'title': "Stippled Stories",
+		'desc': "A bunch of illustrations heavily influenced by sureealism in a mundane world.",
+		'length' : 5
+	},
+
+	"36 days of type 2018 edition":{
+		'title': "36 Days Of Type 2018 Edition",
+		'desc': "An online exercise that takes place every year thru Instagram. The theme followed here was Indian flowers and the typeface was created around it.",
+		'length' : 36
+	},
+
+	"Surface Pattern Design":{
+		'title': "Surface Pattern Design",
+		'desc': "Created a surreal surface pattern in a pen illustrated-stippled style, which was displayed at the prestigious, Premiere Vision (Paris). This exhibition is centered around fashion and these prints were eventually going to be a part of merchandise.",
+		'length' : 6
+	},
+
+
+
+
+
+
+
+
+}
+
+
+
+
